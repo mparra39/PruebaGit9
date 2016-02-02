46c8b9717b6cd195be17318f6701ef23a6b1083d
administrador <administrador@macbook-pro-de-administrador.local>
2016-02-02 10:28:00 -0600

    Initial Commit

---
+++ PruebaGit10Tests/PruebaGit10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PruebaGit10Tests.swift
+//  PruebaGit10Tests
+//
+//  Created by administrador on 02/02/16.
+//  Copyright © 2016 ITESO. All rights reserved.
+//
+
+import XCTest
+@testable import PruebaGit10
+
+class PruebaGit10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
