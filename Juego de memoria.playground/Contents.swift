7b2b8a41fd324f720abd8932a2994ef1d63a6ba2
administrador <administrador@macbook-pro-de-administrador.local>
2016-02-07 21:19:33 -0600

    Mini reto Juego de memoria

---
+++ Juego de memoria.playground/Contents.swift
@@ -0,0 +1,24 @@
+//Juego de memoria
+
+import UIKit
+
+
+for rango in 0...100{
+    //print(rango)
+    
+    if(rango % 5 == 0){                     //Si rango es divisible entre 5
+        print("# \(rango) Bingo!!!")
+    }
+    
+    if(rango % 2 == 0){                     //Determinar si rango es par o impar
+        print("# \(rango) par!!!")
+    }else{
+        print("# \(rango) impar!!!")
+    }
+    
+    if(rango >= 30) && (rango <= 40){       //Si rango se encuentra entre 30 y 40
+        print("# \(rango) Viva Swift!!!")
+    }
+    
+    
+}
