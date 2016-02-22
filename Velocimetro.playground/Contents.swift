e8ccd7185a8d01766cfce0f0e87a16a77fad6582
administrador <mparrah39@gmail.com>
2016-02-21 20:58:06 -0600

    Tarea Velocímetro

--- Velocimetro.playground/Contents.swift
+++ Velocimetro.playground/Contents.swift
@@ -1,7 +1,77 @@
-//: Playground - noun: a place where people can play
+//: Velocímetro
 
 import UIKit
 
-var str = "Hello, playground 2"
+//Enum de Velocidades
+enum Velocidades : Int{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
 
-var prueba = "Hola"
+
+//Clase Auto
+class Auto {
+    
+    var velocidad : Velocidades
+    
+    init(){
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String){
+        
+        var velocidadEnCadena : String
+        var actual : Int
+        
+        switch velocidad{
+            
+        case .Apagado:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Apagado"
+            velocidad = .VelocidadBaja
+        case .VelocidadBaja:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velocidad Baja"
+            velocidad = .VelocidadMedia
+        case .VelocidadMedia:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velocidad Media"
+            velocidad = .VelocidadAlta
+        case .VelocidadAlta:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velocidad Alta"
+            velocidad = .VelocidadMedia
+        }
+        
+        return (actual, velocidadEnCadena)
+    }
+}
+
+
+
+// Pruebas de la clase
+
+//Instancia de la clase Auto
+var auto = Auto()
+
+
+//Itera 20 veces
+for i in 1...20 {
+    print(auto.cambioDeVelocidad())
+}
+
+print("")
+
+//Salida con 5 iteraciones
+var auto1 = Auto()
+
+for i in 1...5{
+    print("\(i). \(auto1.velocidad.rawValue), \(auto1.velocidad) ")
+    auto1.cambioDeVelocidad()
+}
+
+
+
